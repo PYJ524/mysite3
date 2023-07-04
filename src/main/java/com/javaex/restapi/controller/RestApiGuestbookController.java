ead2325650841c54e00e19760a3a000580daa9a9
(ParkYooJun) <(psjun524@naver.com)>
2023-07-04 09:29:48 +0900

    최종수정

--- src/main/java/com/javaex/restapi/controller/RestApiGuestbookController.java
+++ src/main/java/com/javaex/restapi/controller/RestApiGuestbookController.java
@@ -4,17 +4,21 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.javaex.service.GuestbookService;
 import com.javaex.vo.GuestbookVo;
 import com.javaex.vo.JsonResult;
 
-@Controller
+@RestController
 @RequestMapping(value="/restapi")
 public class RestApiGuestbookController {
 
@@ -22,8 +26,7 @@ public class RestApiGuestbookController {
 	private GuestbookService guestbookService;
 	
 	// 전체리스트 가져오기
-	@ResponseBody
-	@RequestMapping(value="/guestbooks" , method= {RequestMethod.GET})
+	@GetMapping(value="/guestbooks")
 	public JsonResult list() {
 		System.out.println("RestApiGuestbookController.list()");
 		
@@ -36,9 +39,7 @@ public class RestApiGuestbookController {
 	}
 	
 	// 게스트북 등록
-	
-	@ResponseBody
-	@RequestMapping(value="/guestbooks", method= {RequestMethod.POST})
+	@PostMapping(value="/guestbooks")
 	public JsonResult writeGuest(@RequestBody GuestbookVo guestbookVo) {
 		System.out.println("RestApiGuestbookController.writeGuest()");
 		System.err.println(guestbookVo);
@@ -49,8 +50,7 @@ public class RestApiGuestbookController {
 		return jsonResult;
 	}
 	
-	@ResponseBody
-	@RequestMapping(value="/guestbooks/{no}", method= {RequestMethod.DELETE})
+	@DeleteMapping(value="/guestbooks/{no}")
 	public JsonResult delete(@PathVariable("no") int no, @RequestBody GuestbookVo guestbookVo) {
 		System.out.println("RestApiGuestbookController.deleteGuest()");
 		System.out.println(no);
